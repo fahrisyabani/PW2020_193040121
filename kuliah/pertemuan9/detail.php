0aa1e7392cf0b4eb3f69dbae73ef1ef6ddbd9f30
fahrisyabani <60495901+fahrisyabani@users.noreply.github.com>
2020-04-18 12:34:32 +0700

    koneksi dan tampil data

--- kuliah/pertemuan9/detail.php
+++ kuliah/pertemuan9/detail.php
@@ -1,5 +1,7 @@
 <?php require 'functions.php'; // ambil id dari url 
-$id = $_GET['id']; $mahasiswa = query("SELECT * FROM mahasiswa WHERE id = $id"?>
+$id = $_GET['id'];
+$m = query("SELECT * FROM mahasiswa WHERE id = $id") ?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -12,29 +14,11 @@ $id = $_GET['id']; $mahasiswa = query("SELECT * FROM mahasiswa WHERE id = $id"?>
 <body>
   <h3>Detail Mahasiswa</h3>
   <ul>
-    <li><img src="img/1.jpg" width="60"></li>
-    <li>NRP : 049303748</li>
-    <li>Nama : Sandhika Galih</li>
-    <li>Email : sandhikagalih@unpas.ac.id</li>
-    <li>Jurusan : Teknik Informatika</li>
-    <li><a href="">ubah</a> | <a href="">hapus</a></li>
-    <li><a href="latihan3.php">Kembali ke daftar mahasiswa</li>
-  </ul>
-  <ul>
-    <li><img src="img/2.jpg" width="60"></li>
-    <li>NRP : 049303748</li>
-    <li>Nama : Sandhika Galih</li>
-    <li>Email : sandhikagalih@unpas.ac.id</li>
-    <li>Jurusan : Teknik Informatika</li>
-    <li><a href="">ubah</a> | <a href="">hapus</a></li>
-    <li><a href="latihan3.php">Kembali ke daftar mahasiswa</li>
-  </ul>
-  <ul>
-    <li><img src="img/3.jpg" width="60"></li>
-    <li>NRP : 049303748</li>
-    <li>Nama : Sandhika Galih</li>
-    <li>Email : sandhikagalih@unpas.ac.id</li>
-    <li>Jurusan : Teknik Informatika</li>
+    <li><img src="img/<?= $m['gambar']; ?>" width="60"></li>
+    <li>NRP : <?= $m['nrp']; ?></li>
+    <li>Nama : <?= $m['nama']; ?></li>
+    <li>Email : <?= $m['email']; ?></li>
+    <li>Jurusan : <?= $m['jurusan']; ?></li>
     <li><a href="">ubah</a> | <a href="">hapus</a></li>
     <li><a href="latihan3.php">Kembali ke daftar mahasiswa</li>
   </ul>
