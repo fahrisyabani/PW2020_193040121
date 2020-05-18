c139aa6887ad6ea2cca1259c6272217a09e22866
fahrisyabani <60495901+fahrisyabani@users.noreply.github.com>
2020-05-19 00:39:53 +0700

    menambahkan koneksi index

---
+++ index.php
@@ -0,0 +1,104 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+
+  <!-- My Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Montserrat:100,400,600&display=swap" rel="stylesheet">
+
+  <!--My CSS-->
+  <link rel="stylesheet" href="css/style.css">
+
+  <title>Halaman Utama</title>
+
+
+</head>
+
+<body>
+
+
+  <div class="container mt-4">
+
+    <div class="card" style="background-color: black">
+      <img src="img/rog1.jpg" class="bg-img">
+      <div class="card-body pl-5 pb-5">
+        <nav class="nav mt-3">
+          <h5 class="mr-5">Click<br>Right</h5>
+          <a class="nav-link" href="#">Home</a>
+          <a class="nav-link" href="#">Schedule</a>
+          <a class="nav-link" href="#">Contact</a>
+        </nav>
+
+        <div class="row main mt-3">
+          <div class="col-md-5">
+            <h1 class="display-3">Buy as <span>Much</span> <br>as Possible</h1>
+            <p>Welcome. kami akan membantu anda untuk mendapatkan barang yang kalian cari dan anda butuhkan.</p>
+            <button class="btn btn-lg">/ Read Now</button>
+          </div>
+        </div>
+
+
+
+        <div class="container">
+
+          <div class="col text-center mt-5">
+            <h2>koneksi</h2>
+          </div>
+        </div>
+
+        <div class="row koneksi mb-5">
+          <div class="col-md">
+            <div class="card">
+              <div class="card-body bg-dark text-white">
+                <h5 class="card-title">Kuliah</h5>
+                <p class="card-text">Halaman menuju folder kuliah <a href="kuliah/index_kuliah.php">klik</a> melanjutkan.</p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md">
+            <div class="card">
+              <div class="card-body bg-dark text-white">
+                <h5 class="card-title">Praktikum</h5>
+                <p class="card-text">Halaman menuju folder praktikum <a href="praktikum/index_praktikum.php">klik</a> melanjutkan.</p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md">
+            <div class="card">
+              <div class="card-body bg-dark text-white">
+                <h5 class="card-title">Tubes</h5>
+                <p class="card-text">Halaman menuju folder tubes <a href="tubes">klik</a> melanjutkan.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+      </div>
+    </div>
+
+
+
+
+
+  </div>
+  </div>
+  </div>
+
+
+  <!-- Optional JavaScript -->
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+
+</html>
---
+++ kuliah/index_kuliah.php
@@ -0,0 +1,244 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+
+  <!-- My Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Montserrat:100,400,600&display=swap" rel="stylesheet">
+
+  <!--My CSS-->
+  <link rel="stylesheet" href="css/style.css">
+
+  <title>index</title>
+</head>
+
+<body>
+
+
+  <div class="container mt-4">
+
+    <div class="card" style="background-color: black">
+      <img src="../img/rog1.jpg" class="bg-img">
+      <div class="card-body pl-5 pb-5">
+        <nav class="nav mt-3">
+          <h5 class="mr-5">Click<br>Right</h5>
+          <a class="nav-link" href="#">Home</a>
+          <a class="nav-link" href="#">Schedule</a>
+          <a class="nav-link" href="#">Contact</a>
+        </nav>
+
+        <div class="row main mt-3">
+          <div class="col-md-5">
+            <h1 class="display-3">Buy as <span>Much</span> <br>as Possible</h1>
+            <p>Welcome. kami akan membantu anda untuk mendapatkan folder yang kalian cari dan anda butuhkan.</p>
+            <button class="btn btn-lg">/ Read Now</button>
+          </div>
+        </div>
+
+
+
+        <div class="container">
+
+          <div class="col text-center mt-5">
+            <h2>koneksi</h2>
+          </div>
+        </div>
+
+        <div class="row koneksi mb-5">
+          <div class="col-md">
+            <div class="card">
+              <div class="card-body bg-dark text-white">
+                <h5 class="card-title">Pertemuan 1</h5>
+                <p class="card-text">Halaman menuju folder kuliah <a href="pertemuan1/latihan1.php">lat 1</a> melanjutkan.</p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md">
+            <div class="card">
+              <div class="card-body bg-dark text-white">
+                <h5 class="card-title">Pertemuan 2</h5>
+                <p class="card-text">Halaman menuju folder kuliah
+                  <a href="pertemuan2/latihan1a.php">lat 1a </a>
+                  | <a href="pertemuan2/latihan1b.php">lat 1b </a>
+                  | <a href="pertemuan2/latihan1c.php">lat 1c </a>
+                  melanjutkan.</p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md">
+            <div class="card">
+              <div class="card-body bg-dark text-white">
+                <h5 class="card-title">pertemuan 3</h5>
+                <p class="card-text">Halaman menuju folder kuliah
+                  <a href="pertemuan3/latihan2a.php">lat 2a</a>
+                  | <a href="pertemuan3/latihan2b.php">lat 2b </a>
+                  | <a href="pertemuan3/latihan2c.php">lat 2c </a>
+                  | <a href="pertemuan3/latihan2d.php">lat 2d </a>
+                  | <a href="pertemuan3/latihan2e.php">lat 2e </a>
+                  | <a href="pertemuan3/pengkondisian.php">pengkondisian </a>
+                  | <a href="pertemuan3/pengulangan.php">pengulangan </a>
+                  melanjutkan.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="row koneksi mb-5">
+          <div class="col-md">
+            <div class="card">
+              <div class="card-body bg-dark text-white">
+                <h5 class="card-title">Pertemuan 4</h5>
+                <p class="card-text">Halaman menuju folder kuliah
+                  <a href="pertemuan4/hitungkubus.php">hit kubus </a>
+                  | <a href="pertemuan4/hitunglingkaran.php">hit lingkaran </a>
+                  | <a href="pertemuan4/latihan1.php">lat 1c </a>
+                  | <a href="pertemuan4/str_replace.php">str_replace </a>
+
+                  melanjutkan.</p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md">
+            <div class="card">
+              <div class="card-body bg-dark text-white">
+                <h5 class="card-title">Pertemuan 5</h5>
+                <p class="card-text">Halaman menuju folder kuliah
+                  <a href="pertemuan5/latihanarray.php">array </a>
+                  | <a href="pertemuan5/latihanarrayBulan.php">arrayBulan </a>
+                  melanjutkan.</p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md">
+            <div class="card">
+              <div class="card-body bg-dark text-white">
+                <h5 class="card-title">pertemuan 6</h5>
+                <p class="card-text">Halaman menuju folder kuliah
+                  <a href="pertemuan6/Foreach.php">foreach</a>
+                  | <a href="pertemuan6/latihan4a.php">lat 4a</a>
+                  | <a href="pertemuan6/latihan4b.php">lat 4b </a>
+                  | <a href="pertemuan6/latihan4c.php">lat 4c </a>
+                  | <a href="pertemuan6/latihan4d.php">lat 4d </a>
+                  | <a href="pertemuan6/latihanArrayAssosiative.php">ArrayAssosiative </a>
+                  | <a href="pertemuan6/Tugas2.php">Tugas 2</a>
+
+                  melanjutkan.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="row koneksi mb-5">
+          <div class="col-md">
+            <div class="card">
+              <div class="card-body bg-dark text-white">
+                <h5 class="card-title">Pertemuan 7</h5>
+                <p class="card-text">Halaman menuju folder kuliah
+                  <a href="pertemuan7/Form.php">form </a>
+                  | <a href="pertemuan7/kalkulator.php">kalkulator </a>
+                  | <a href="pertemuan7/Result.php">Result </a>
+                  | <a href="pertemuan7/ResultKalkulator.php">ResultKalkulator </a>
+
+                  melanjutkan.</p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md">
+            <div class="card">
+              <div class="card-body bg-dark text-white">
+                <h5 class="card-title">Pertemuan 9</h5>
+                <p class="card-text">Halaman menuju folder kuliah
+                  <a href="pertemuan9/pw_193040121.sql">sql </a>
+
+                  melanjutkan.</p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md">
+            <div class="card">
+              <div class="card-body bg-dark text-white">
+                <h5 class="card-title">pertemuan 10</h5>
+                <p class="card-text">Halaman menuju folder kuliah
+                  <a href="pertemuan10/detail.php">detail</a>
+                  | <a href="pertemuan10/functions.php">functions</a>
+                  | <a href="pertemuan10/latihan1.php">lat 1 </a>
+                  | <a href="pertemuan10/latihan2.php">lat 2 </a>
+                  | <a href="pertemuan10/latihan3.php">lat 3 </a>
+                  | <a href="pertemuan10/tambah.php">tambah data </a>
+
+
+                  melanjutkan.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+        <div class="row koneksi mb-5">
+          <div class="col-md">
+            <div class="card">
+              <div class="card-body bg-dark text-white">
+                <h5 class="card-title">Pertemuan 11</h5>
+                <p class="card-text">Halaman menuju folder kuliah
+                  <a href="pertemuan11/index.php"> klik</a>
+
+                  melanjutkan.</p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md">
+            <div class="card">
+              <div class="card-body bg-dark text-white">
+                <h5 class="card-title">Pertemuan 12</h5>
+                <p class="card-text">Halaman menuju folder kuliah
+                  <a href="pertemuan12/login.php"> klik </a>
+
+                  melanjutkan.</p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md">
+            <div class="card">
+              <div class="card-body bg-dark text-white">
+                <h5 class="card-title">pertemuan 13</h5>
+                <p class="card-text">Halaman menuju folder kuliah
+                  <a href="pertemuan13/login.php"> klik </a>
+
+
+                  melanjutkan.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+      </div>
+
+
+    </div>
+  </div>
+  </div>
+
+
+  <!-- Optional JavaScript -->
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+
+</html>
